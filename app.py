af36b22544d33c874d2932d387627156cfd0f5f7
Kelvin Purdom <kelvin.purdom@gmail.com>
2025-06-24 15:10:13 +0200

    updated header

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from src.utils import validate_csv_file
 RESULTS_PATH = 'data/results_housing_class.csv'
 
 def main():
+    st.markdown("### 🏡 Housing ML Model Dashboard")
     st.title('🏠 Housing Classification App 🏠' )
     st.write('Welcome to the housing classification app. Please enter your first name below, then upload your results file to check your accuracy and see the leaderboard.')
     st.write('🤖 Good Luck and remember it is just for fun!! 🤖')
